03e3cacf0288111c0e589c2e09abb4184248830c
rikho septian <mghy25rikho@gmail.com>
2023-12-28 20:18:04 +0700

    Update Terbaru

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Teruntuk kamu :</div>
+
+                    <div class="card-body">
+                    {{ message }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Cuma Mau Bilang Sok Semangat🎉🎉🎉'
+            };
+        },
+        props() {
+            nama: string
+            description: string
+            stoc: number
+        },
+    };
+</script>
---
+++ resources/js/components/IndexComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Disclaimer : {{ nama }}</div>
+
+                    <div class="card-body">
+                        {{ message }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            nama: 'Rikho Septian',
+            message: 'Selamat anda sudah berhasil menjalankan laravel + vue.',
+        };
+    },
+};
+</script>
